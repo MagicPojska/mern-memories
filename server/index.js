959073996c9c64cdd821e255b17b1797f6ebb6ce
MagicPojska <pojskicsafet@gmail.com>
2021-10-19 16:09:57 +0200

    Added enviroment variables

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
@@ -12,12 +13,10 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://mrrobot:CescFabregas4@cluster0.ydhym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () => console.log(`Server ruinning on port: ${PORT}`))
   )
