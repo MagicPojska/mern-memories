751064a1480905ea1a4028bd5a60b3bee13e8e06
mrrobot <pojskicsafet@gmail.com>
2021-12-01 13:57:53 +0100

    Removed localhost api url

--- client/src/api/index.jsx
+++ client/src/api/index.jsx
@@ -1,9 +1,10 @@
-import axios from 'axios'
+import axios from "axios";
 
-const url = 'http://localhost:8080/posts' || 'https://mern-memories-p.herokuapp.com/posts'
+const url = "https://mern-memories-p.herokuapp.com/posts";
 
-export const fetchPosts = () => axios.get(url)
-export const createPost = (newPost) => axios.post(url, newPost)
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost)
-export const deletePost = (id) => axios.delete(`${url}/${id}`)
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`)
+export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost);
+export const updatePost = (id, updatedPost) =>
+  axios.patch(`${url}/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
