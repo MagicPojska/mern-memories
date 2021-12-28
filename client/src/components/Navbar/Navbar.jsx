962635959fc396afe96c1f750c77ff242e8e45c7
mrrobot <pojskicsafet@gmail.com>
2021-12-28 13:49:56 +0100

    Added logout function

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,19 +3,30 @@ import { Link } from "react-router-dom";
 import useStyles from "./Styles";
 import memories from "../../images/memories.png";
 import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 const Navbar = () => {
   const classes = useStyles();
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-  useEffect(() => {
-    const token = user?.token;
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
 
-    //JWT Here
+    history.push("/");
+    setUser(null);
+  };
 
-    setUser(JSON.parse(localStorage.getItem("profile")));
-  }, []);
-  console.log(user);
+  //   useEffect(() => {
+  //     const token = user?.token;
+
+  //     //JWT Here
+
+  //     setUser(JSON.parse(localStorage.getItem("profile")));
+  //   }, []);
+
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
@@ -50,6 +61,7 @@ const Navbar = () => {
               variant="contained"
               className={classes.logut}
               color="secondary"
+              onClick={logout}
             >
               Logout
             </Button>
