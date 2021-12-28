a421866c04bb516b1a872b776bce101cd0ccbce7
mrrobot <pojskicsafet@gmail.com>
2021-12-28 15:36:12 +0100

    Added interceptor for api calls with bearer token

--- client/src/api/index.jsx
+++ client/src/api/index.jsx
@@ -4,6 +4,15 @@ const API = axios.create({ baseURL: "http://localhost:8080" });
 
 const url = "https://mern-memories-p.herokuapp.com/posts";
 
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem("profile")) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).token
+    }`;
+  }
+  return req;
+});
+
 export const fetchPosts = () => API.get("/posts");
 export const createPost = (newPost) => API.post("/posts", newPost);
 export const updatePost = (id, updatedPost) =>
