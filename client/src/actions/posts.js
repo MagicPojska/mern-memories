8e245934ca9ce89e106513f59fba79960811cd26
mrrobot <pojskicsafet@gmail.com>
2021-12-29 13:53:24 +0100

    Added search functionality

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,5 +1,11 @@
 import * as api from "../api";
-import { FETCH_ALL, CREATE, UPDATE, DELETE } from "../constants/actionTypes";
+import {
+  FETCH_ALL,
+  FETCH_BY_SEARCH,
+  CREATE,
+  UPDATE,
+  DELETE,
+} from "../constants/actionTypes";
 // Action creators
 
 export const getPosts = () => async (dispatch) => {
@@ -18,7 +24,7 @@ export const getPostsBySearch = (searchQuery) => async (dispatch) => {
       data: { data },
     } = await api.fetchPostsBySearch(searchQuery);
 
-    console.log(data);
+    dispatch({ type: FETCH_BY_SEARCH, payload: data });
   } catch (error) {
     console.log(error);
   }
